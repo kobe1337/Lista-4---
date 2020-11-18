3e01603a5f2012f2c82eeef2b2718d215c8c54ee
kobe1337 <viniciusmota1337@gmail.com>
2020-11-18 11:03:38 +0000

    k

--- Exerc8.py
+++ Exerc8.py
@@ -2,9 +2,11 @@
 #Aluno: Paulo Vinicius Mota - 2014290074
 #Lista 4 - Exercicio 8
 x = 1
-while x == 0:
-  print(x)
-  if x>x:
-    r = x;
-  x = x+1
+y = 0
+while x > 0:
+  x = int(input("Digite um numero inteiro:"))
+  
+  if x>y:
+    y = x;
+    print(y)
   
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #Disciplina: Probabilidade e Estatistica
 #Aluno: Paulo Vinicius Mota - 2014290074
 #Lista 4 - Exercicio 8
+
+#obs: So estava conseguindo testar os meus codigos nesse arquivo main. portanto, se caso nao funcione rodando os arquivos diretos tente copiar o codigo e colar aqui que vai funcionar corretamente
 x = 1
+y = 0
 while x > 0:
   x = int(input("Digite um numero inteiro:"))
-  y = x
+  if x>y:
+    y = x;
+    print(y)
   
