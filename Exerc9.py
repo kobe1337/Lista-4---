6d6334aff5be36c9d97bbeb88da69a17a5c1f379
kobe1337 <viniciusmota1337@gmail.com>
2020-11-18 20:02:15 +0000

    finish

--- Exerc9.py
+++ Exerc9.py
@@ -2,3 +2,16 @@
 #Aluno: Paulo Vinicius Mota - 2014290074
 #Lista 4 - Exercicio 9
 
+x= int(input('Digite o número: '))#ler os valor a ser calculado
+
+mil = x // 1000 % 10 #Faz o operação de retirada do numero de milhar
+cen = x // 100 % 10 #Faz o operação de retirada do numero de centena
+dez = x // 10 % 10 #Faz o operação de retirada do numero de dezena
+uni = x // 1 % 10 #Faz o operação de retirada do numero de unidade
+
+
+print("Numero inteiro {}".format(x)) #Imprimi o numero inteiro
+print("Milhar {}".format(mil)) #Imprimi o numero correspondente ao milhar
+print("Centena {}".format(cen)) #Imprimi o numero correspondente a centena
+print("Dezena {}".format(dez)) #Imprimi o numero correspondente a dezena
+print("Unidade {}".format(uni)) #Imprimi o numero correspodente a unidade
--- main.py
+++ main.py
@@ -3,12 +3,3 @@
 #Lista 4 - Exercicio 8
 
 #obs: So estava conseguindo testar os meus codigos nesse arquivo main. portanto, se caso nao funcione rodando os arquivos diretos tente copiar o codigo e colar aqui que vai funcionar corretamente
-
-l1 = [] #Lista para inserção de valores
-while True:#Repetição sem parametro de encerramento
-    x= int(input('Digite o número (0 para encerrar): '))#ler os valores
-    l1.append(x)#Lista com o parametro da inserção de valores
-    if x == 0:#Condição para encerramento da repetição
-        break
-print ('O maior número da lista é: ',max(l1))#imprimi o maior numero da lista
-  
